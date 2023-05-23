cce7b08a2480f05ac0c242dd8607391ab5126999
WilsonFilho2 <wilson.menezes09@aluno.ifce.edu.br>
2023-05-23 16:39:32 -0300

    login e cadastro

--- data/urls.py
+++ data/urls.py
@@ -4,4 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     
+    path('login', views.login, name='login'),
+    path('cadastro', views.cadastro, name='cadastro'),
+    path('logout', views.logout, name='logout'),
 ]
--- data/views.py
+++ data/views.py
@@ -1,6 +1,52 @@
 from django.shortcuts import render
-# from .models import ...
+from django.http import HttpResponseRedirect, request
+from django.urls import reverse
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login as django_login, logout as django_logout
+
 
 # Create your views here.
 def index(request):
+    if not request.user.is_authenticated:
+        return HttpResponseRedirect(reverse('login'))
     return render(request, 'data/index.html')
+
+def login(request):
+
+    if request.method == 'POST':
+        username = request.POST.get('name')
+        password = request.POST.get('password')
+
+        user = authenticate(request, username = username, password = password)
+
+        if user:
+            django_login(request, user)
+            return HttpResponseRedirect(reverse('index'))
+        
+        else:
+            return HttpResponseRedirect(reverse('login'))
+
+    return render(request, 'data/login.html')
+
+def cadastro(request):
+
+    if request.method == 'POST':      
+        username = request.POST.get('name')
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+
+        try:
+            user = User.objects.create_user(username=username, email=email, password=password)
+            user.save() 
+            user = authenticate(request, username=username, password=password)
+            django_login(request, user)
+            return HttpResponseRedirect(reverse('index'))
+        
+        except:
+            return HttpResponseRedirect(reverse('login'))
+
+    return render(request, 'data/cadastro.html')
+
+def logout(request):
+    django_logout(request)
+    return HttpResponseRedirect(reverse('login'))
