48928b47e1c62c0cb284218ddd0f21cc38ebf028
Ramon-Venancio <ramonvenanciosousa@gmail.com>
2023-06-18 12:36:33 -0300

    funcionamento do grafico

--- data/regressao.py
+++ data/regressao.py
@@ -17,7 +17,7 @@ def estimate_coef(x, y):
 	b_1 = SS_xy / SS_xx
 	b_0 = m_y - b_1*m_x
 
-	return (b_0, b_1)
+	return [b_0, b_1]
 
 def plot_regression_line(x, y, b):
 	# plotting the actual points as scatter plot
@@ -36,3 +36,19 @@ def plot_regression_line(x, y, b):
 
 	# function to show plot
 	plt.show()
+
+def main():
+	# observations / data
+	x = np.array([0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
+	y = np.array([1, 3, 2, 5, 7, 8, 8, 9, 10, 12])
+
+	# estimating coefficients
+	b = estimate_coef(x, y)
+	print("Estimated coefficients:\nb_0 = {} \
+		\nb_1 = {}".format(b[0], b[1]))
+
+	# plotting regression line
+	plot_regression_line(x, y, b)
+
+if __name__ == "__main__":
+	main()
